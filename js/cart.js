8a8e1e1d48bf304dc39b0cfbe38a88166782d448
szz <1316225239@qq.com>
2017-12-06 20:29:09 +0800

    完成商品详情页基本布局，放大镜显示

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,13 @@
+//加载头尾
 require(["config"],function(){
 		require(["jquery","loadHeaderFooter"],function(){
 		});
 	});
+
+// 加载放大镜插件
+require(["config"],function(){
+	require(["jquery","zoom"],function($){
+		console.log($("big_img"));
+		$(".big_img img").elevateZoom();
+	});
+});
--- js/config.js
+++ js/config.js
@@ -4,6 +4,12 @@ require.config({
 		"jquery" : ["https://code.jquery.com/jquery-1.12.4.min","letao/lib/jquery/jquery-1.12.4.min"],
 		"cookie" : "letao/lib/jquery_plugins/jquery.cookie",
 		"template" : "letao/lib/arttemplate/template",
-		"loadHeaderFooter" : "letao/js/loadHeaderFooter"
+		"loadHeaderFooter" : "letao/js/loadHeaderFooter",
+		"zoom" : "letao/lib/jquery_plugins/jquery.elevatezoom"
+	},
+	shim : {
+		"zoom" : {
+			deps : ["jquery"]
+		}
 	}
 });
